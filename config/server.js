698ca85fa5752a3314636a0992a8184552ca5512
amira-zm <amiragsm4@icloud.com>
2021-07-12 16:31:47 +0100

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '70d62612c2fac4023e0d467618842335'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5050f76f-55e9-4ed6-8329-5032deadc07b'
+};
